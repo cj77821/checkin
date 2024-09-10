368a4bef419e0f6de8005deca3756f9cce12737e
cj77821 <116792885+cj77821@users.noreply.github.com>
2024-09-10 09:11:39 +0800

    Update main.js

--- main.js
+++ main.js
@@ -54,8 +54,8 @@ const notify = async (contents) => {
 
 const main = async () => {
   await notify(await glados("GLADOS", "2424"))
-  await notify(await glados("GLADOS_4108", "4108"))
-  await notify(await glados("GLADOS_cool", "cool"))
+  // await notify(await glados("GLADOS_4108", "4108"))
+  // await notify(await glados("GLADOS_cool", "cool"))
 }
 
 main()
